a23374c038175d9c7ca745d52b138129d199e163
lbeck37 <lbeck37@gmail.com>
2023-12-02 15:34:20 -0800

    Working on first tasking program

---
+++ IDF_Workspaces/B32_Len2First/main/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <unistd.h>
+
+void app_main(void)
+{
+    //while (true) {
+    while (1) {
+        printf("app_main(): Runs! 12/2/23a\n");
+        //sleep(1);
+        sleep(3);
+    }
+}

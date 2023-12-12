faa6f54fcb2e89aa84db773042ab1474060107d7
lbeck37 <lbeck37@gmail.com>
2023-12-11 18:39:10 -0800

    Got B32_TTGOTCoupleRemote going without board hooked up

--- Arduino/Sketches/B32_TCoupleRemote/B32_TCoupleRemote.ino
+++ Arduino/Sketches/B32_TCoupleRemote/B32_TCoupleRemote.ino
@@ -1,5 +1,5 @@
 char szSketchName[]  = "B32_TCoupleRemote.ino";
-char szFileDate[]    = "10/19/23e";
+char szFileDate[]    = "12/11/23b";
 /* MAX31855 library example sketch
  This sample code is designed to be used on the MAX31855x8 breakout board.
  The board has a single MAX31855 IC on it, and uses a multiplexer
@@ -32,6 +32,8 @@ char szFileDate[]    = "10/19/23e";
 //communication code from the esp_now.h library.
 #include <B32_TCoupleLib.h>
 
+const bool  bNoTCouples     = true;
+
 uint8_t aucReceiverMACAddress[]= {0xB0, 0xB2, 0x1C, 0x4F, 0x28, 0x0C};   //BlackPin MAC
 
 //ESP32 GPIO pin numbers (range from 0 to 39)
@@ -46,6 +48,8 @@ uint8_t aucReceiverMACAddress[]= {0xB0, 0xB2, 0x1C, 0x4F, 0x28, 0x0C};   //Black
 MAX31855 TCoupleObject = MAX31855(MISO, CS, SCK);
 
 double  dJunctionDegF;
+double  dDummyJunctionDegF  = 60.00;
+double  dDummyAddDegF       =  0.00;
 
 //Function prototypes
 void  setup                   (void);
@@ -94,12 +98,19 @@ void SetupPins(void){
 
 
 void ReadAmbiant(void){
-  dJunctionDegF= TCoupleObject.readJunction(FAHRENHEIT);
+  if (!bNoTCouples){
+    dJunctionDegF= TCoupleObject.readJunction(FAHRENHEIT);
+  }   //if(!bNoTCouples)
+  else{
+    dDummyJunctionDegF += 0.10;
+    dJunctionDegF= dDummyJunctionDegF; 
+  }   //if(!bNoTCouples)else
   return;
 } //ReadAmbiant
 
 
 void ReadTCouples(void){
+/*
   //Read the temperatures of the 8 thermocouples
   for (int wTCoupleNum=0; (wTCoupleNum < wNumTCouples); wTCoupleNum++) {
     //Select the thermocouple
@@ -112,6 +123,31 @@ void ReadTCouples(void){
 
     //adTCoupleDegF[wTCoupleNum]= TCoupleObject.readThermocouple(FAHRENHEIT);
     stOutgoingReadings.adTCoupleDegF[wTCoupleNum]= TCoupleObject.readThermocouple(FAHRENHEIT);
+    if (stOutgoingReadings.adTCoupleDegF[wTCoupleNum] == FAULT_OPEN){
+      //Break out of for loop, go to top of for loop and next TCouple
+      continue;
+    } //if(stOutgoingReadings.adTCoupleDegF[wTCoupleNum]==FAULT_OPEN)
+  }   //for(int wTCoupleNum=0;wTCoupleNum<8;wTCoupleNum++)
+*/
+  //Read the temperatures of the 8 thermocouples
+  for (int wTCoupleNum=0; (wTCoupleNum < wNumTCouples); wTCoupleNum++) {
+    //Select the thermocouple
+    digitalWrite(T0, wTCoupleNum & 1? HIGH: LOW);
+    digitalWrite(T1, wTCoupleNum & 2? HIGH: LOW);
+    digitalWrite(T2, wTCoupleNum & 4? HIGH: LOW);
+    //The MAX31855 takes 100ms to sample the TCouple.
+    //Wait a bit longer to be safe.  We'll wait 0.125 seconds
+    delay(125);
+
+    if (!bNoTCouples){
+      //adTCoupleDegF[wTCoupleNum]= TCoupleObject.readThermocouple(FAHRENHEIT);
+      stOutgoingReadings.adTCoupleDegF[wTCoupleNum]= TCoupleObject.readThermocouple(FAHRENHEIT);
+    } //if(!bNoTCouples)
+    else{
+      dDummyAddDegF += 0.10;
+      stOutgoingReadings.adTCoupleDegF[wTCoupleNum]= (100.00 + (wTCoupleNum * 10.00) + dDummyAddDegF);
+    } //if(!bNoTCouples)else
+  
     if (stOutgoingReadings.adTCoupleDegF[wTCoupleNum] == FAULT_OPEN){
       //Break out of for loop, go to top of for loop and next TCouple
       continue;

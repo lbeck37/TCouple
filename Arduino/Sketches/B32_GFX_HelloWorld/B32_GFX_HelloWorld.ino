719d56fb3fe117e0c9652b5f5205bb4c652a0e52
lbeck37 <lbeck37@gmail.com>
2024-01-20 16:12:29 -0800

    Creating B32_GFX_HelloWorld.ino to see if we can get Waveshare parallel 800x480 ESP32 w/display

--- Arduino/Sketches/B32_GFX_HelloWorld/B32_GFX_HelloWorld.ino
+++ Arduino/Sketches/B32_GFX_HelloWorld/B32_GFX_HelloWorld.ino
@@ -1,5 +1,5 @@
 const char szSketchName[]  = "B32_GFX_HelloWorld.ino";
-const char szFileDate[]    = "1/16/24b";
+const char szFileDate[]    = "1/20/24b";
 
 /*******************************************************************************
  * Start of Arduino_GFX setting
@@ -42,6 +42,9 @@ const char szFileDate[]    = "1/16/24b";
 #else
 #define GFX_BL DF_GFX_BL // default backlight pin, you may replace DF_GFX_BL to actual backlight pin
 
+const uint16_t  usPanelWidth      = 800;
+const uint16_t  usPanelHeight     = 480;
+
 const int8_t    cDE_Pin           =  5;
 const int8_t    cVsyncPin         =  3;
 const int8_t    cHsyncPin         = 46;
@@ -51,14 +54,47 @@ const int8_t    acRedPin[]        = { 1,  2, 42, 41, 40};
 const int8_t    acBluePin[]       = {14, 38, 18, 17, 10};
 const int8_t    acGreenPin[]      = {39,  0, 45, 48, 47, 21};
 
+const uint16_t  usHsyncPolarity   = 0;
+const uint16_t  usVsyncPolarity   = 0;
+
+const uint16_t  usHsyncFrontPorch = 0;
+const uint16_t  usVsyncFrontPorch = 0;
+
+const uint16_t  usHsyncPulseWidth = 0;
+const uint16_t  usVsyncPulseWidth = 0;
+
+const uint16_t  usHsyncBackPorch  = 0;
+const uint16_t  usVsyncBackPorch  = 0;
+
+const uint16_t  usPclkActiveNeg   = 0;
+
+const uint32_t  uwPreferSpeed     = 0;
+
+const bool      bUseBigEndian     = false;
+
+const uint16_t  usDEIdleHigh      = 0;
+const uint16_t  usPclkIdleHigh    = 0;
+
+/*
+const int8_t    cRotation         = 0;
+const bool      bAutoFlush        = true;
+Arduino_DataBus *pArduino_DataBus = NULL;
+*/
+
 /* More data bus class: https://github.com/moononournation/Arduino_GFX/wiki/Data-Bus-Class */
-Arduino_DataBus *bus = create_default_Arduino_DataBus();
+Arduino_ESP32RGBPanel *pRGBPanel = new Arduino_ESP32RGBPanel(
+        cDE_Pin      , cVsyncPin    , cHsyncPin    , cPclkPin,
+        acRedPin[0]  , acRedPin[1]  , acRedPin[2]  , acRedPin[3]  , acRedPin[4],
+        acGreenPin[0], acGreenPin[1], acGreenPin[2], acGreenPin[3], acGreenPin[4], acGreenPin[5],
+        acBluePin[0] , acBluePin[1] , acBluePin[2] , acBluePin[3] , acBluePin[4],
+        usHsyncPolarity, usHsyncFrontPorch, usHsyncPulseWidth, usHsyncBackPorch,
+        usVsyncPolarity, usVsyncFrontPorch, usVsyncPulseWidth, usVsyncBackPorch,
+        usPclkActiveNeg, uwPreferSpeed    , bUseBigEndian    , usDEIdleHigh    , usPclkIdleHigh);
 
 
-/* More display class: https://github.com/moononournation/Arduino_GFX/wiki/Display-Class */
-Arduino_GFX *gfx = new Arduino_ILI9341(bus, DF_GFX_RST, 0 /* rotation */, false /* IPS */);
-
+Arduino_RGB_Display *pRGBDisplay = new Arduino_RGB_Display(usPanelWidth, usPanelHeight, pRGBPanel);
 #endif
+
 
 void setup(void)
 {
@@ -72,30 +108,32 @@ void setup(void)
 #endif
 
   // Init Display
-  if (!gfx->begin())
+  if (!pRGBDisplay->begin())
   {
-    Serial.println("gfx->begin() failed!");
+    Serial.println("pRGBDisplay->begin() failed!");
   }
-  gfx->fillScreen(BLACK);
+  pRGBDisplay->fillScreen(BLACK);
 
 #ifdef GFX_BL
   pinMode(GFX_BL, OUTPUT);
   digitalWrite(GFX_BL, HIGH);
 #endif
 
-  gfx->setCursor(10, 10);
-  gfx->setTextColor(RED);
-  gfx->println("Hello World!");
+  pRGBDisplay->setCursor(10, 10);
+  pRGBDisplay->setTextColor(RED);
+  pRGBDisplay->println("Hello World!");
 
   delay(5000); // 5 seconds
-}
+} //setup
+
 
 void loop()
 {
-  gfx->setCursor(random(gfx->width()), random(gfx->height()));
-  gfx->setTextColor(random(0xffff), random(0xffff));
-  gfx->setTextSize(random(6) /* x scale */, random(6) /* y scale */, random(2) /* pixel_margin */);
-  gfx->println("Hello World!");
+  pRGBDisplay->setCursor(random(pRGBDisplay->width()), random(pRGBDisplay->height()));
+  pRGBDisplay->setTextColor(random(0xffff), random(0xffff));
+  pRGBDisplay->setTextSize(random(6) /* x scale */, random(6) /* y scale */, random(2) /* pixel_margin */);
+  pRGBDisplay->println("Hello World!");
 
   delay(1000); // 1 second
-}
+} //loop
+//Last line.

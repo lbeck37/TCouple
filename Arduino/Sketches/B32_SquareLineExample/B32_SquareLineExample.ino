8f602225f7e11560d93492ed8a9725ec6f76b996
lbeck37 <lbeck37@gmail.com>
2024-02-10 17:26:40 -0800

    Got the code generated in SquareLine Studio and after moving .c image and screen files into src/

--- Arduino/Sketches/B32_SquareLineExample/B32_SquareLineExample.ino
+++ Arduino/Sketches/B32_SquareLineExample/B32_SquareLineExample.ino
@@ -1,8 +1,9 @@
 const char szSketchName[]  = "B32_SquareLineExample.ino";
-const char szFileDate[]    = "2/8/24g";
+const char szFileDate[]    = "2/10/24J";
 //Use Arduino_GFX as driver for LVGL calls to write to Waveshare 800x480, 4.3" 16-bit 5-6-5 RGB
 
 #include <B32_LVGL_DriverLib.h>
+#include <ui.h>
 #include <Streaming.h>
 
 #ifndef BLOG
@@ -24,8 +25,19 @@ void setup(){
 
   Serial << BLOG << " setup(): Call SetupLVGL" << endl;
   SetupLVGL();
+  delay(100);      //I saw a crash in the printout
 
+  Serial << BLOG << " setup(): Call DisplayFooter" << endl;
   DisplayFooter();
+  lv_timer_handler(); /* let the GUI do its work */
+  delay(2000);      //Give a chance to read the sketch, date and MAC
+
+/*
+  Serial << BLOG << " setup(): Call ui_init" << endl;
+  ui_init();
+  delay(100);
+*/
+
   Serial << BLOG << " setup(): Done" << endl;
   return;
 } //setup
@@ -42,7 +54,7 @@ void DisplayFooter(void){
   char  szLabelText[90];
   char  szMACText  [30];
 
-  Serial << BLOG << " setup(): Call DisplayLabel" << endl;
+  Serial << BLOG << " DisplayFooter(): Call DisplayLabel" << endl;
   sprintf(szLabelText, "%s, %s, MAC=%s", szSketchName, szFileDate, szGetMyMAC(szMACText));
   DisplayLabel(szLabelText);
   return;
